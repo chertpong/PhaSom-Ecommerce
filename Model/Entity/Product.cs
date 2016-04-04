18ec2eaea7e3e5ff57987f382eca97adcf6daa20
idefgh <phanaphon_s@cmu.ac.th>
2016-04-04 15:30:53 +0700

    add entity-product

---
+++ Model/Entity/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entity
+{
+    class Product
+    {
+        private long Id { get; set; }
+        private string Name { get; set; }
+        private double Price { get; set; }
+        private string Description { get; set; }
+        private string Thumbnail { get; set; }
+        private List<string> Tags { get; set; }
+        private int Amount { get; set; }
+    }
+}
