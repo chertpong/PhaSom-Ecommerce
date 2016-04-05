81c138efc56790cad85ee7ad60c8a1bf07edf1a6
caramaii <caramaii@hotmail.com>
2016-04-05 23:56:02 +0700

    Add DAL (Data Access Layer) folder in Model, add Seed.cs to test Database data

---
+++ Model/DAL/Seed.cs
@@ -0,0 +1,31 @@
+﻿using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAL
+{
+    public class StoreInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<StoreContext>
+    {
+        protected override void Seed(StoreContext context)
+        {
+            var products = new List<Product>
+            {
+                new Product(1,"Rice",25.00,"Thailand people eat rice berry","12345",new List<string> {"EatType"}, 10),
+                new Product(2,"Shampoo",50.00,"Washing Hair","5463",new List<string> {"Usetype"}, 19),
+                new Product(3,"Coffee",1500.00,"Drink for Energy","1777",new List<string> {"EatType"}, 30),
+                new Product(4,"KraTiam",60.00,"Cooking Material","1777",new List<string> {"EatType"}, 16),
+                new Product(5,"Bag",300.00,"Keep everthing inside","1777",new List<string> {"Usetype"}, 5)
+
+            };
+
+            products.ForEach(s => context.Products.Add(s));
+            context.SaveChanges();
+            {
+            }
+        }
+
+    }
+}
---
+++ Model/DAL/StoreContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Model.Entity;
+
+
+namespace Model.DAL
+{
+    public class StoreContext : DbContext
+    {
+
+        public StoreContext() : base("StoreContext")
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
