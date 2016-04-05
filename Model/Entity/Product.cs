5bbd010ee33f8cd71fd3e2befd8364647474b05b
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-04-05 22:58:50 +0700

    Add Constructor In Product. Add Mock in AddBindings() NinjectControllerFactory.

--- Model/Entity/Product.cs
+++ Model/Entity/Product.cs
@@ -15,5 +15,16 @@ namespace Model.Entity
         private string Thumbnail { get; set; }
         private List<string> Tags { get; set; }
         private int Amount { get; set; }
+
+       public Product(long Id , string Name , double Price , string Description , string Thumbnail, List<string> Tags , int Amount)
+       {
+           this.Id = Id;
+           this.Name = Name;
+           this.Price = Price;
+           this.Description = Description;
+           this.Thumbnail = Thumbnail;
+           this.Tags = Tags;
+           this.Amount = Amount;
+       }
     }
 }
