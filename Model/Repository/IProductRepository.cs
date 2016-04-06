606e8983ac65b4ba3ed07ec00812fb921647abc5
idefgh <phanaphon_s@cmu.ac.th>
2016-04-06 17:53:07 +0700

    rebuild IProductRepository

--- Model/Repository/IProductRepository.cs
+++ Model/Repository/IProductRepository.cs
@@ -10,8 +10,12 @@ namespace Model.Repository
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
-        
-      
+        IEnumerable<Product> GetAll();
+        Product Get(string customerID);
+        Product Add(Product item);
+        void Remove(string customerID);
+        bool Update(Product item);
+
 
     }
 }
