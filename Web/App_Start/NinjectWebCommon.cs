d476efb6e4a5dcc742c60203f8c5904c912e1ffe
chertpong_h <chertpong_h@cmu.ac.th>
2016-04-23 00:52:25 +0700

    Fix ninject

--- Web/App_Start/NinjectWebCommon.cs
+++ Web/App_Start/NinjectWebCommon.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using Model.Entity;
+using Model.Repository;
+using Moq;
+
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Web.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Web.App_Start.NinjectWebCommon), "Stop")]
 
@@ -61,6 +66,18 @@ namespace Web.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.GetAll()).Returns(new List<Product>
+            {
+                new Product { Id = 1,Name = "Rice",Price = 25.00,Description = "Thailand people eat rice berry",Thumbnail = "12345",Tags = new List<string> {"EatType"}, Amount = 10},
+                new Product { Id = 2,Name = "Shampoo",Price = 50.00,Description = "Washing Hair",Thumbnail = "5463",Tags = new List<string> {"Usetype"},Amount = 19},
+                new Product { Id = 3,Name = "Coffee",Price = 1500.00,Description = "Drink for Energy",Thumbnail = "1777",Tags = new List<string> {"EatType"},Amount = 30},
+                new Product { Id = 4,Name = "KraTiam",Price = 60.00,Description = "Cooking Material",Thumbnail = "1788",Tags = new List<string> {"EatType"},Amount = 16},
+                new Product { Id = 5,Name = "Bag",Price = 300.00,Description = "Keep everthing inside",Thumbnail = "1789",Tags = new List<string> {"Usetype"},Amount = 5}
+
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }        
     }
 }
--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -6,7 +6,6 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Web.Infrastructure;
 
 namespace Web
 {
@@ -19,9 +18,6 @@ namespace Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory());
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
