ce090da2c4bbf444638fe16bbd7ee01e151d2413
yong <phanaphon_s@cmu.ac.th>
2016-04-25 17:18:48 +0700

    fix ProductController

--- Web/Controllers/ProductController.cs
+++ Web/Controllers/ProductController.cs
@@ -1,48 +1,46 @@
-﻿using Model.Entity;
-using Model.Repository;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Web;
+using System.Net.Http;
 using System.Web.Http;
-using System.Web.Http.Results;
-using System.Web.Mvc;
-
+using Model.Repository;
+using Model.Entity;
 namespace Web.Controllers
 {
     public class ProductController : ApiController
     {
-        private readonly IProductRepository _repository;
+        private IProductRepository _repository;
 
-        public ProductController()
+        public ProductController(IProductRepository repository)
+        {
+            this._repository = repository;
+        }
+        // GET: api/Product
+        public IEnumerable<Product> Get()
+        {
+            return _repository.GetProducts();
+        }
+
+        // GET: api/Product/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        public void Post([FromBody]string value)
         {
         }
 
-        public ProductController(IProductRepository repositoryProduct)
+        // PUT: api/Product/5
+        public void Put(int id, [FromBody]string value)
         {
-            this._repository = repositoryProduct;
         }
 
-        public IEnumerable<Product> GetAllProduct()
+        // DELETE: api/Product/5
+        public void Delete(int id)
         {
-            return _repository.GetAll();
-        }
-
-        public Product GetProduct(int productID)
-        {
-            var product = _repository.Get(productID);
-            if (product == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-            return product;
-        }
-
-        public Product PostProduct(Product products)
-        {
-            var product = _repository.Add(products);
-            return product;
         }
     }
 }
