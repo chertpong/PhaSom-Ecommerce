1af2ec4b0008d620d2eb9b3dd2c942ce6e9fc5e8
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-04-30 09:24:37 +0700

    Add LoginTest

---
+++ Web.Tests/Service/DemoTestLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Web.Controllers;
+
+namespace Web.Tests.Service
+{
+    public class DemoTestLogin
+    {
+        public DemoTestLogin()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestCase()]
+        public void testLogin1()
+        {
+            Assert.IsTrue();
+        }
+    }
+}
