c2be40e9a19088d859a197bba4c3c25cd22a6abd
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-05-02 16:27:25 +0700

    Add Post Repository

---
+++ Model/Repository/IPostRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Model.Entity;
+
+namespace Model.Repository
+{
+    public interface IPostRepository : IDisposable
+    {
+        List<Post> GetAll();
+        Post GetPostById(int id);
+        void CreatePost(Post post);
+        void UpdatePost(Post post);
+        void DeletePost(int id);
+
+        List<Post> Find(Expression<Func<Post, bool>> predicate)
+    }
+}
