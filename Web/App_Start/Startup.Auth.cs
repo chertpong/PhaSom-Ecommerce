ee2946144b79a00973fabf127e5e39407b7ce449
chertpong_h <chertpong_h@cmu.ac.th>
2016-05-04 00:54:07 +0700

    Add  function addRoleToUser

--- Web/App_Start/Startup.Auth.cs
+++ Web/App_Start/Startup.Auth.cs
@@ -8,6 +8,7 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using Web.Infrastructure;
 using Web.Providers;
 using Web.Models;
 
@@ -25,6 +26,8 @@ namespace Web
             // Configure the db context and user manager to use a single instance per request
             app.CreatePerOwinContext(ApplicationDbContext.Create);
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
--- Web/Controllers/RoleController.cs
+++ Web/Controllers/RoleController.cs
@@ -4,9 +4,14 @@ using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Web.Models;
+using Microsoft.AspNet.Identity.Owin;
+using Web.Infrastructure;
 
 namespace Web.Controllers
 {
@@ -14,11 +19,27 @@ namespace Web.Controllers
     public class RoleController : ApiController
     {
         private readonly ApplicationDbContext _context;
+        private ApplicationUserManager _userManager;
+        private ApplicationRoleManager _roleManager;
+
+        public ApplicationUserManager UserManager
+        {
+            get { return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+            private set { _userManager = value; }
+        }
+
+        public ApplicationRoleManager RoleManager
+        {
+            get { return _roleManager ?? Request.GetOwinContext().GetUserManager<ApplicationRoleManager>(); }
+            private set { _roleManager = value; }
+        }
 
         public RoleController(ApplicationDbContext context)
         {
             _context = context;
         }
+
+      
 
         // GET: api/Role
         public List<IdentityRole> Get()
@@ -34,17 +55,19 @@ namespace Web.Controllers
         }
 
         // POST: api/Role
-        public bool Post([FromBody]string name)
+        public bool Post([FromBody]RoleViewModels.CreateRoleViewModel role)
         {
             // TODO : Add Exception handler
-            _context.Roles.Add(new IdentityRole() {Name = name});
+            _context.Roles.Add(new IdentityRole() {Name = role.Name});
             return _context.SaveChanges() > 0;
         }
 
         // PUT: api/Role/5
-        public bool Put(string id, [FromBody]IdentityRole role)
+        public bool Put(string id, [FromBody]string name)
         {
             // TODO : Add Exception handler
+            var role = _context.Roles.First(r => r.Id.Equals(id));
+            role.Name = name;
             _context.Entry(role).State = EntityState.Modified;
             return _context.SaveChanges() > 0;
         }
@@ -55,6 +78,16 @@ namespace Web.Controllers
             // TODO : Add Exception annotation handler
             var role = _context.Roles.First(r => r.Id.Equals(id));
             _context.Roles.Remove(role);
+        }
+        [Route("api/Role/AddRoleToUser")]
+        [HttpPost]
+        public async Task<bool> AddRoleToUser([FromBody]RoleViewModels.AddRoleToUserViewModel model)
+        {
+            var role = await this.RoleManager.FindByNameAsync(model.RoleName);
+            var user = await this.UserManager.FindByEmailAsync(model.Username);
+            System.Diagnostics.Debug.WriteLine(role.Name);
+            var result = await this.UserManager.AddToRolesAsync(user.Id, new string[] { role.Name });
+            return result.Succeeded;
         }
     }
 }
