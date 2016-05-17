a9933db9f77146fc2ad5434cd0fccaf2e756b275
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-05-17 11:53:51 +0700

    Fix product Service test

--- Web.Tests/Service/ProductServiceTest.cs
+++ Web.Tests/Service/ProductServiceTest.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -37,17 +39,17 @@ namespace Web.Tests.Service
             p4 = new Product { Id = 4, Name = "Rice Berry", Price = 70.00, Description = "For Eat", Thumbnail = "AaAA564.com", Tags = new List<string> { "Edible" }, Amount = 40 };
             p5 = new Product { Id = 5, Name = "Pan", Price = 500.00, Description = "For Use", Thumbnail = "Pan.com", Tags = new List<string> { "Usable" }, Amount = 70 };
             productRepository = new Mock<IProductRepository>();
-            productRepository.Setup(repository => repository.GetAll()).Returns(new List<Product> { p1 });
+            productRepository.Setup(repository => repository.GetAll()).Returns(new List<Product> { p1, p2, p3, p4, p5 });
         }
 
         [Test]
         public void SearchProductbyName()
         {
             var service = new ProductService(productRepository.Object);
-            CollectionAssert.AreEqual(new List<Product> { p1 }, service.SearchProductbyName("Rice"));
-            //CollectionAssert.AreEqual(new List<Product> { p1,p4 }, service.SearchProductbyName("R"));
-            //CollectionAssert.AreEqual(new List<Product> {p1,p2,p3,p4,p5}, service.SearchProductbyName(""));
-            //CollectionAssert.AreEqual(new List<Product> {} , service.SearchProductbyName("yong")  );
+            CollectionAssert.AreEqual(new List<Product> { p1 , p4 }, service.SearchProductbyName("Rice"));
+            CollectionAssert.AreEqual(new List<Product> { p1,p4 }, service.SearchProductbyName("R"));
+            CollectionAssert.AreEqual(new List<Product> {p1,p2,p3,p4,p5}, service.SearchProductbyName(""));
+            CollectionAssert.AreEqual(new List<Product> { } , service.SearchProductbyName("yong")  );
 
         }
 
@@ -55,10 +57,19 @@ namespace Web.Tests.Service
         public void SearchProductByTag()
         {
             var service = new ProductService(productRepository.Object);
-            //CollectionAssert.Contains(new List<Product> {p1}, service.SearchProductByTag("Edible"));
-            CollectionAssert.AreEqual(new List<Product> { p1, p3, p4 }, service.SearchProductByTag("Edible"));
-            // CollectionAssert.Contains(new List<Product> { p5 }, service.SearchProductByTag("Usable"));
-            //CollectionAssert.AreEqual(new List<Product> { p2,p5 }, service.SearchProductByTag("Usable"));
+            //CollectionAssert.Contains(new List<Product> { p1 }, service.SearchProductByTag("Edible"));
+            //CollectionAssert.AreEqual(new List<Product> { p1, p3, p4 }, service.SearchProductByTag("Edible"));
+            //CollectionAssert.Contains(new List<Product> { p5 }, service.SearchProductByTag("Usable"));
+            CollectionAssert.AreEqual(new List<Product> { p2,p5 }, service.SearchProductByTag("Usable"));
+        }
+
+        [Test]
+        public void p0oiCreateProductShouldPass()
+        {
+            var service = new ProductService(productRepository.Object);
+            Product p6 = new Product {Name = "Garlic",Price = 60.00,Description = "Raw material for cook.",Thumbnail = "Garlic.com",Tags = new List<string> { "Edible" },Amount = 60};
+            service.Create(p6);
+            productRepository.Verify(p => p.Create(p6));
         }
 
 
