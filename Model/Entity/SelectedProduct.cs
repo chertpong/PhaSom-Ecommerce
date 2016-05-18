8f45b937a47c438b43fe66a2a5c9a1d8cd9c522b
phanaphon <phanaphon_s@cmu.ac.th>
2016-05-18 15:59:32 +0700

    add entity

---
+++ Model/Entity/SelectedProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entity
+{
+ public  class SelectedProduct
+    {
+     public int Id { get; set; }
+     public int Amount { get; set; }
+     public double Price { get; set; }
+     public Product Product { get; set; }
+     public double CalculatePrice { get; set; }
+
+    }
+}
