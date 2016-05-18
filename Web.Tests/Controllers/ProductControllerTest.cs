60fa5847ba57a74c1502ed2f62ee695160f75ef6
devilsjackiez <jackrin_pat@cmu.ac.th>
2016-05-18 22:36:59 +0700

    Add Product Controller Test

---
+++ Web.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model.Entity;
+
+namespace Web.Tests.Controllers
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void GetAllProducts_ShouldReturnAllProducts()
+        {
+            
+        }
+    }
+}
