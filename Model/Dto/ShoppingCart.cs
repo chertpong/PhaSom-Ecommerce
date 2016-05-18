007382b775e8c9b6aa814bac6a62bc22ef983689
phanaphon <phanaphon_s@cmu.ac.th>
2016-05-18 15:58:50 +0700

    add ShoppingCartDto

---
+++ Model/Dto/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Entity;
+
+namespace Model.Dto
+{
+    public class ShoppingCart
+    {
+        public DateTime Updated { get; set; }
+        public List<SelectedProduct> SelectedProducts { get; set; }
+    }
+}
