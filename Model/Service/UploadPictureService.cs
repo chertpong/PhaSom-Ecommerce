a47e83ae6058b2612b980d9ef2c6956ae4c1085d
PHANAPHONSUWANMANEE <phanaphon_s@cmu.ac.th>
2016-05-19 22:49:34 +0700

    upload picture not complete

---
+++ Model/Service/UploadPictureService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using NUnit.Engine.Internal;
+
+namespace Model.Service
+{
+   public class UploadPictureService
+    {
+       public void UploadPicture(HttpPostedFileBase file)
+       {
+           if (file != null && file.ContentLength > 0)
+           {
+
+               String timestamp = DateTime.Now.ToString("HH:mm:ss.fff",
+                   System.Globalization.DateTimeFormatInfo.InvariantInfo);
+               String name = file.FileName + timestamp;
+               String ext = Path.GetExtension(file.FileName);
+               String fname = name.GetHashCode().ToString() + ext;
+
+               string path = Path.Combine(Server("~/Images"), fname);
+               file.SaveAs(path);
+
+           }
+
+
+       }
+    }
+}
---
+++ Web/Controllers/TestUploadAPIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Model.Service;
+
+namespace Web.Controllers
+{
+    public class TestUploadAPIController : ApiController
+    {
+        UploadPictureService test = new UploadPictureService();
+
+        public void upload(HttpPostedFileBase file)
+        {
+             test.UploadPicture(file);
+        }
+    }
+}
